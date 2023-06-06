12e816cd3046d1dd0323bb4189910d42d759e685
hottabych <varvar1987a@mail.ru>
2023-06-06 23:51:50 +0300

    social_network

--- wey_backend/chat/admin.py
+++ wey_backend/chat/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Conversation, ConversationMessage
 
 
-admin.site.register(Conversation)
 
+admin.site.register(Conversation)
 admin.site.register(ConversationMessage)
